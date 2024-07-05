8358b9b4df1c941d101a0671e139242202e54a75
dyabo <dyabo.box@gmail.com>
2024-07-05 09:05:41 +0200

    canvas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import _ from "lodash";
-import Puzzle from "./100.png";
-import "./style.css";
+import imageSrc from "./100.png"; // Import the image file
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported with a script
-  element.innerHTML = _.join(["Hello", "code100"], " ");
-  element.classList.add("image");
-
-  const InnerLayer = new Puzzle();
-  InnerLayer.src = Puzzle;
-
-  element.appendChild(InnerLayer);
+  // Create an Image element and set its source
+  const myImage = new Image();
+  myImage.src = imageSrc; // Use the imported image URL as the src
+  element.appendChild(myImage); // Append the image to the div element
 
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component()); // Append the div element to the body
+
+// TODO!!
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(
+  24, //x center
+  160, //y center
+  30, //r
+  0, //start angle
+  Math.PI * 2, //end angle
+  false, //drawn direction
+);
+ctx.fillStyle = "purple";
+ctx.fill();
+ctx.closePath();
